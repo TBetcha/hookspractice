496bb945b8124bfdbc653d9bb7bfd2f75b2add76
Tbetcha <troy.boettger@gmail.com>
2020-06-10 23:40:19 -0400

    initial commit

---
+++ src/components/TodoList.js
@@ -0,0 +1,33 @@
+/** @format */
+
+import React, { useContext } from 'react'
+import TodosContext from '../context'
+
+export default function TodoList() {
+	//we'll get back what we passed to TodosContext from index.js
+	// state and dispatch
+	const { state, dispatch } = useContext(TodosContext)
+	const title = state.todos.length > 0 ? `${state.todos.length} Todos` : 'Nothing to do'
+	return (
+		<div className='container mx-auto mx-w-md text-center font-mono'>
+			<h1 className='text-bold'>{title}</h1>
+			<ul className='list-reset text-white p-0'>
+				{state.todos.map((todo) => (
+					<li
+						style={{ background: 'orange' }}
+						key={todo.id}
+						className='flex items-center border-black border-dashed border-2 my-2 py-4'
+					>
+						<span className='cursor-pointer flex-1 ml-12'>{todo.text}</span>
+						<button>
+							<img src='https://icon.now.sh/edit/0050c5' alt='Edit Icon' className='h6' />
+						</button>
+						<button>
+							<img src='https://icon.now.sh/delete/8b000' alt='Edit Icon' className='h-6' />
+						</button>
+					</li>
+				))}
+			</ul>
+		</div>
+	)
+}
---
+++ src/context.js
@@ -0,0 +1,15 @@
+/** @format */
+
+import React from 'react'
+
+const TodosContext = React.createContext({
+	todos: [
+		{ id: 1, text: 'eat breakfast', complete: false },
+
+		{ id: 2, text: 'finsih project ', complete: true },
+
+		{ id: 3, text: 'make bed', complete: false },
+	],
+})
+
+export default TodosContext
